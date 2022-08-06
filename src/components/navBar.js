05109c1d3e72d6db131d4ba8edc72a97a6d3ad6a
Niran S Narayanan <nirans2002@gmail.com>
2022-08-06 11:46:07 +0530

    typo corrected in id

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -51,7 +51,7 @@ function navBar() {
           <Nav className="ms-auto">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
