083498555fd7ae99fddd5069f5399c0816a9cc76
Anirudh A V <anirudh.av02@gmail.com>
2022-10-02 23:13:07 +0530

    Team page completed > All components of the team page are completed. The team page is now fully responsive. > Need to add the team members' pictures and their respective links to their social media accounts.

--- src/components/MemberPanel.js
+++ src/components/MemberPanel.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './css/MemberPanel.css';
 
-import {MemberCard, Chairperson} from './MemberCard';
+import { MemberCard, Chairperson } from './MemberCard';
 import Anirudh_A_V from '../assets/team/Anirudh_A_V.png';
 
 const MemberPanel = () => {
     return (
         <div className="member-panel">
-            <div className="member-panel-heading">
+            {/* <div className="member-panel-heading">
                 <h1>Meet Our Team</h1>
-            </div>
+            </div> */}
             <div className="member-team">
                 <h2>Team</h2>
             </div>
             <div className="member-panel-details">
-                Consectetur sit duis consequat ex fugiat cupidatat. In nulla minim incididunt nostrud et esse do nulla ut. Consectetur veniam Lorem do officia ipsum fugiat nulla et consequat elit. Aliqua dolore duis cillum adipisicing consequat ex enim veniam duis cupidatat ipsum proident.Consectetur sit duis consequat ex fugiat cupidatat. In nulla minim incididunt nostrud et esse do nulla ut. Consectetur veniam Lorem do officia ipsum fugiat nulla et consequat elit. Aliqua dolore duis cillum adipisicing consequat ex enim veniam duis cupidatat ipsum proident.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget egestas purus viverra accumsan in nisl. Eu non diam phasellus vestibulum lorem sed risus. A iaculis at erat pellentesqu
             </div>
             <div className="member-chair">
-            <Chairperson name="Anirudh A V" position="Founder" image={Anirudh_A_V} linkedin="anirudh-a-v" github=""></Chairperson>
+                <Chairperson name="Anirudh A V" position="Founder" image={Anirudh_A_V} linkedin="anirudh-a-v" github=""></Chairperson>
             </div>
             <div className="member-panel-cards">
+                <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
+                <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
@@ -28,18 +30,22 @@ const MemberPanel = () => {
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
             </div>
             <div className="member-team">
-                <h2>Core Team</h2>
+                <h2>Co Team</h2>
             </div>
             <div className="member-panel-details">
-                Consectetur sit duis consequat ex fugiat cupidatat. In nulla minim incididunt nostrud et esse do nulla ut. Consectetur veniam Lorem do officia ipsum fugiat nulla et consequat elit. Aliqua dolore duis cillum adipisicing consequat ex enim veniam duis cupidatat ipsum proident.Consectetur sit duis consequat ex fugiat cupidatat. In nulla minim incididunt nostrud et esse do nulla ut. Consectetur veniam Lorem do officia ipsum fugiat nulla et consequat elit. Aliqua dolore duis cillum adipisicing consequat ex enim veniam duis cupidatat ipsum proident.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget egestas purus viverra accumsan in nisl. Eu non diam phasellus vestibulum lorem sed risus. A iaculis at erat pellentesqu
             </div>
-            <div className="member-panel-cards">
+            <div className="member-panel-cards co-team">
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
                 <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
+                <MemberCard name="Anirudh A V" position="Technical coordinator" image={Anirudh_A_V} linkedin="anirudh-a-v" github="" size="medium"></MemberCard>
+            </div>
+            <div className="member-panel-details end-para">
+                Porta non pulvinar neque laoreet suspendisse interdum. Eu volutpat odio facilisis mauris sit amet. Nisl purus in mollis nunc sed id semper risus. Enim diam vulputate ut pharetra. Volutpat blandit aliquam etiam erat. Tellus cras adipiscing enim eu turpis egestas. Rhoncus dolor purus non enim praesent elementum facilisis. Placerat vestibulum lectus mauris ultrices eros in. Pharetra sit amet aliquam id diam maecenas. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Pretium quam vulputate dignissim suspendisse in est ante.
             </div>
         </div>
 
@@ -47,3 +53,4 @@ const MemberPanel = () => {
 }
 
 export default MemberPanel;
+
