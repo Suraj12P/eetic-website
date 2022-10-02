cd60b60ebfa7e36312303e5bad1e3fa53192af78
Anirudh A V <anirudh.av02@gmail.com>
2022-10-02 19:11:05 +0530

    chairperson component created

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './css/MemberCard.css';
 import {IoLogoLinkedin} from 'react-icons/io';
 
-const MemberCard = ({ name, position, image, linkedin, github }) => {
+const MemberCard = ({ name, position, image, linkedin, github, size }) => {
     return (
         <div className="member-card">
             <div className="member-card-image">
@@ -13,7 +13,6 @@ const MemberCard = ({ name, position, image, linkedin, github }) => {
                 <p>{position}</p>
                 <div className="member-card-socials">
                     <a href={`https://www.linkedin.com/in/${linkedin}/`} target="_blank" rel="noreferrer">
-                        {/* <i className="fab fa-linkedin"></i> */}
                         <IoLogoLinkedin size="1.5em"  color="white"/>  
                     </a>
                     {/* <a href={github} target="_blank" rel="noreferrer">
@@ -25,4 +24,26 @@ const MemberCard = ({ name, position, image, linkedin, github }) => {
     );
 }
 
-export default MemberCard;
+const Chairperson = ({ name, position, image, linkedin, github }) => {
+    return (
+        <div className="chair-member-card">
+            <div className="chair-member-card-image">
+                <img src={image} alt="chair-member" />
+            </div>
+            <div className="chair-member-card-details">
+                <h3>{name}</h3>
+                <p>{position}</p>
+                <div className="chair-member-card-socials">
+                    <a href={`https://www.linkedin.com/in/${linkedin}/`} target="_blank" rel="noreferrer">
+                        <IoLogoLinkedin size="1.5em"  color="white"/>  
+                    </a>
+                    {/* <a href={github} target="_blank" rel="noreferrer">
+                        <i className="fab fa-github"></i>
+                    </a> */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export {MemberCard, Chairperson};
