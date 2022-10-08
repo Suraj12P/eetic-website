f9a7050a344b87d17e4cb86378330aa5890b56dc
Niran S Narayanan <nirans2002@gmail.com>
2022-10-08 21:49:45 +0530

    sign in ip commented

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,37 +1,4 @@
-// import React from "react";
-// import './css/navBar.css';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Button from "react-bootstrap/Button";
-// function navBar(){
-//     return(
-//       <div className="navbox">
-//       <Navbar>
-
-//          <div className="logo l1">
-//          EETICommunity
-//          </div>
-
-//          <div className="logo l2">
-//          EETIC
-//          </div>
-//          <div className="logo l3">
-//          ommunity
-//          </div>
-//          <div className="links">
-//            <div className="home mouse">Home</div>
-//            <div className="events mouse">Events</div>
-//            <div className="about mouse">About Us</div>
-//            <div className="team mouse">Team</div>
-//            </div>
-//            <div className="signIn mouse">Sign in</div>
-//         <Button>Sign Up</Button>
-//       </Navbar>
-//       </div>
-//     );
-// }
-
-// export default navBar;
-
+// NAVBAR 
 /* ============================================================================================================== */
 
 import React from "react";
@@ -100,12 +67,12 @@ function navBar() {
                    */}
              
 
-            <Nav.Link href="#signIn" className="navText">
+            {/* <Nav.Link href="#signIn" className="navText">
               <Button className="signInUpButton signIn">Sign In</Button>
             </Nav.Link>
             <Nav.Link href="#signUp" className="navText ">
               <Button className=" signUp signInUpButton">Sign Up</Button>
-            </Nav.Link>
+            </Nav.Link> */}
 
             {/* ====================================================== */}
           </Nav>
