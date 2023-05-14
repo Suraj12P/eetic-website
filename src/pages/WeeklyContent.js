c32dbe2214b3a2bf5afcdb2176bbeb9fbcfa38c5
Srutav Tarun <srutavtarun@gmail.com>
2023-05-14 18:27:04 +0530

    minor changes

---
+++ src/pages/WeeklyContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/navBar';
+import Footer from '../components/footer';
+
+function WeeklyContent() {
+  return (
+    <div>
+        <NavBar />
+            <h1>Weekly Content</h1>
+        <Footer />
+    </div>
+  )
+}
+
+export default WeeklyContent
