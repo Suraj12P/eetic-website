68e24f6df97895e541c52f27ee0fad6fc09fa41d
Adithyan T <adithsreedhar628@gmail.com>
2023-05-15 20:25:58 +0530

    updated  weekly content

--- src/pages/WeeklyContent.js
+++ src/pages/WeeklyContent.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import "./Style/WeeklyContent.css";
 import NavBar from "../components/navBar";
 import Footer from "../components/footer";
-import week1 from "./Images/week1.jpg"
-import week2 from "./Images/week2.jpg"
+import week1 from "./Images/week1.jpg";
+import week2 from "./Images/week2.jpg";
 
 function WeeklyContent() {
   //   const instagramPostUrl =
@@ -26,31 +26,37 @@ function WeeklyContent() {
   //       console.error(error);
   //     });
 
+  const weeklyContent = [
+    {
+      title: "Week 1",
+      path : week1,
+      link : "https://www.instagram.com/p/CrqlNVipRgM/?igshid=YmMyMTA2M2Y%3D "
+    },
+    {
+      title: "Week 2",
+      path : week2,
+      link : "https://www.instagram.com/p/CsNg-u4plHd/?igshid=NTc4MTIwNjQ2YQ%3D%3D"
+    },
+
+  ]
+
   return (
-    <div>
+    <div className="weeklycontent">
       <NavBar />
       <div className="WCmain">
         <div className="WCheading">Weekly Content</div>
         <div className="WCrow">
-          <div className="WCcard">
-            <a
-              href="https://www.instagram.com/p/CrqlNVipRgM/?igshid=YmMyMTA2M2Y%3D"
-              target="_blank"
-            >
-              <div className="WCweek-img"><img src={week1} alt="Week-1" className="WCweek_1" /></div>
-            </a>
-            <div className="WCweek-text">Week 1</div>
-          </div>
-          <div className="WCcard">
-            <a
-              href="https://www.instagram.com/p/CsNg-u4plHd/?igshid=NTc4MTIwNjQ2YQ%3D%3D"
-              target="_blank"
-            >
-              <div className="WCweek-img"><img src={week2} alt="Week-2" className="WCweek_2" /></div>
-            </a>
-            <div className="WCweek-text">Week 2</div>
-          </div>
-          <div className="WCnot-card">ㅤ</div>
+          {weeklyContent.map((item) => (
+            <div className="WCcard">
+              <a 
+                href={item.link}
+                target="_blank"
+              >
+                <div className="WCweek-img"><img src={item.path} alt={item.title} className="WCweek_1" /></div>
+              </a>
+              <div className="WCweek-text">{item.title}</div>
+            </div>
+          ))}
         </div>
       </div>
       <Footer />
