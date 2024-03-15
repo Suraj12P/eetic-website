43525b844271f4b1e0ca1dac352e238ff7d65b0c
Akash M Nandan <akash003nandan@gmail.com>
2024-03-16 01:23:20 +0530

    banner height and width set

--- src/components/Discord_Banner.js
+++ src/components/Discord_Banner.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import './css/discord_banner.css'
 
 const Discord_Banner = () => {
   return (
-    <div>Discord_Banner</div>
+    <div className='banner'>
+      <a href="https://discord.gg/KAbXKEXG" className='link-text'>Join Our Discord Community</a>
+    </div>
   )
 }
 
